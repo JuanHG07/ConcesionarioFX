978b6e16c126280acc2d21afef24282b4ea43888
Sebastian-Mateus <juans.mateush@uqvirtual.edu.co>
2024-11-17 15:45:25 -0500

    Datos

---
+++ conceseionariofx/src/main/java/co/edu/uniquindio/poo/viewController/DatosVehiculoViewController.java
@@ -0,0 +1,730 @@
+package co.edu.uniquindio.poo.viewController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.event.ActionEvent;
+
+public class DatosVehiculoViewController {
+
+    @FXML
+    private TextField txtAbsCamion;
+
+    @FXML
+    private TextField txtAbsVan;
+
+    @FXML
+    private TextField txtCapacidadMaleteroCamioneta;
+
+    @FXML
+    private TextField txtNumeroBolsasAirePickUp;
+
+    @FXML
+    private TextField txtAireAcondicionadoPickUp;
+
+    @FXML
+    private RadioButton radioVan;
+
+    @FXML
+    private TextField txtNumeroPasajerosVan;
+
+    @FXML
+    private TextField txtTiempoCargaElectrico;
+
+    @FXML
+    private RadioButton radioMoto;
+
+    @FXML
+    private TextField txtNumeroBolsasAireDeportivo;
+
+    @FXML
+    private TextField txtAireAcondicionadoVan;
+
+    @FXML
+    private TextField txtCamaraReversaVan;
+
+    @FXML
+    private TextField txtNumeroBolsasAireCamioneta;
+
+    @FXML
+    private TextField txtSensorTraficoCruzadoSedan;
+
+    @FXML
+    private TextField txtNumeroPuertasVan;
+
+    @FXML
+    private TextField txtNumeroEjesBus;
+
+    @FXML
+    private TextField txtEnchufableHibrido;
+
+    @FXML
+    private TextField txtCamaraReversaSedan;
+
+    @FXML
+    private TextField txtAutonomiaElectrico;
+
+    @FXML
+    private TextField txtNumeroBolsasAireSedan;
+
+    @FXML
+    private TextField txtNumeroPasajerosPickUp;
+
+    @FXML
+    private RadioButton radioSinCombustion;
+
+    @FXML
+    private RadioButton radioDiesel;
+
+    @FXML
+    private TextField txtNumeroPuertasDeportivo;
+
+    @FXML
+    private RadioButton radioManual;
+
+    @FXML
+    private RadioButton radioBus;
+
+    @FXML
+    private RadioButton radioHibrido;
+
+    @FXML
+    private TextField txtNumeroPuertasPickUp;
+
+    @FXML
+    private TextField txtNumeroPasajerosBus;
+
+    @FXML
+    private TextField txtNumeroPasajerosCamioneta;
+
+    @FXML
+    private TextField txtCodigo;
+
+    @FXML
+    private TextField txtSensorColisionCamioneta;
+
+    @FXML
+    private TextField txtAireAcondicionadoBus;
+
+    @FXML
+    private TextField txtCapacidadCargaPickUp;
+
+    @FXML
+    private RadioButton radioAutomatico;
+
+    @FXML
+    private TextField txtNumeroEjes;
+
+    @FXML
+    private TextField txtNumeroBolsasAireVan;
+
+    @FXML
+    private Button btnBuscar;
+
+    @FXML
+    private TextField txtCaballosFuerzaDeportivo;
+
+    @FXML
+    private RadioButton radioCamion;
+
+    @FXML
+    private TextField txtDefensas;
+
+    @FXML
+    private TextField txtTraccionCamioneta;
+
+    @FXML
+    private TextField txtNumeroPuertasSedan;
+
+    @FXML
+    private TextField txtVelocidadCruceroCamioneta;
+
+    @FXML
+    private RadioButton radioGasolina;
+
+    @FXML
+    private TextField txtNumeroPasajerosSedan;
+
+    @FXML
+    private TextField txtCapacidadMaleteroVan;
+
+    @FXML
+    private TextField txtAsistentePermanenciaCarrilSedan;
+
+    @FXML
+    private TextField txtAsistentePermanenciaCarrilCamioneta;
+
+    @FXML
+    private TextField txtNumeroPuertasCamioneta;
+
+    @FXML
+    private TextField txtVelocidadCruceroBus;
+
+    @FXML
+    private TextField txtNuevo;
+
+    @FXML
+    private TextField txtAireAcondicionadoCamion;
+
+    @FXML
+    private TextField txtAireAcondicionadoCamioneta;
+
+    @FXML
+    private TextField txtTiempoCeroCienDeportivo;
+
+    @FXML
+    private TextField txtFrenosAire;
+
+    @FXML
+    private TextField txtCamaraReversaCamioneta;
+
+    @FXML
+    private TextField txtTraccionPickUp;
+
+    @FXML
+    private TextField txtEspejosElectricosSedan;
+
+    @FXML
+    private TextField txtNumeroBolsasAireBus;
+
+    @FXML
+    private TextField txtAbsBus;
+
+    @FXML
+    private TextField txtNumeroPasajerosDeportivo;
+
+    @FXML
+    private TextField txtCapacidadMaleteroBus;
+
+    @FXML
+    private Button btnActualizar;
+
+    @FXML
+    private TextField txtAbsSedan;
+
+    @FXML
+    private TextField txtNumeroSalidasEmergenciaBus;
+
+    @FXML
+    private TextField txtModelo;
+
+    @FXML
+    private Button btnLimpiar;
+
+    @FXML
+    private TextField txtCapacidadCargaCamion;
+
+    @FXML
+    private TextField txtAireAcondicionadoSedan;
+
+    @FXML
+    private RadioButton radioCamioneta;
+
+    @FXML
+    private TextField txtAbsPickUp;
+
+    @FXML
+    private TextField txtLigeroHibrido;
+
+    @FXML
+    private VBox vboxTipoVehiculo;
+
+    @FXML
+    private TextField txtSensorColisionSedan;
+
+    @FXML
+    private Button btnSalir;
+
+    @FXML
+    private TextField txtAbsCamioneta;
+
+    @FXML
+    private TextField txtCambios;
+
+    @FXML
+    private TextField txtVelMax;
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private RadioButton radioSedan;
+
+    @FXML
+    private RadioButton radioElectrico;
+
+    @FXML
+    private Button btnEliminar;
+
+    @FXML
+    private RadioButton radioPickUp;
+
+    @FXML
+    private TextField txtVelocidadCruceroSedan;
+
+    @FXML
+    private TextField txtCapacidadMaleteroSedan;
+
+    @FXML
+    private TextField txtCilindraje;
+
+    @FXML
+    private TextField txtNumeroPuertasBus;
+
+    @FXML
+    private TextField txtMarca;
+
+    @FXML
+    private RadioButton radioDeportivo;
+
+    @FXML
+    private TextField txtSensorTraficoCruzadoCamioneta;
+
+    @FXML
+    void txtCodigoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtMarcaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtModeloAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNuevoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCambiosAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtVelMaxAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCilindrajeAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTipoVehiculoSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTipoVehiculoSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTipoVehiculoSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTipoVehiculoSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTipoVehiculoSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTipoVehiculoSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTipoVehiculoSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTipoVehiculoSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTipoVehiculoSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTipoVehiculoSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtDefensasAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCapacidadCargaCamionAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAireAcondicionadoCamionAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtFrenosAireAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAbsCamionAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroEjesAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroPasajerosBusAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroPuertasBusAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroBolsasAireBusAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAireAcondicionadoBusAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAbsBusAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtVelocidadCruceroBusAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCapacidadMaleteroBusAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroSalidasEmergenciaBusAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroEjesBusAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroPasajerosVanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroPuertasVanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroBolsasAireVanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAireAcondicionadoVanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAbsVanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCamaraReversaVanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCapacidadMaleteroVanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroPasajerosPickUpAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroPuertasPickUpAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroBolsasAirePickUpAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAireAcondicionadoPickUpAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAbsPickUpAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtTraccionPickUpAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCapacidadCargaPickUpAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroPasajerosDeportivoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroPuertasDeportivoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroBolsasAireDeportivoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCaballosFuerzaDeportivoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtTiempoCeroCienDeportivoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroPasajerosSedanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroPuertasSedanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroBolsasAireSedanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAireAcondicionadoSedanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAbsSedanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtVelocidadCruceroSedanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCapacidadMaleteroSedanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCamaraReversaSedanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSensorColisionSedanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSensorTraficoCruzadoSedanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAsistentePermanenciaCarrilSedanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtEspejosElectricosSedanAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroPasajerosCamionetaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroPuertasCamionetaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNumeroBolsasAireCamionetaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAireAcondicionadoCamionetaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAbsCamionetaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtVelocidadCruceroCamionetaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCapacidadMaleteroCamionetaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCamaraReversaCamionetaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSensorColisionCamionetaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSensorTraficoCruzadoCamionetaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAsistentePermanenciaCarrilCamionetaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAutonomiaElectricoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtTiempoCargaElectricoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtEnchufableHibridoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtLigeroHibridoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void radioGasolinaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void radioDieselAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void radioSinCombustionAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void radioAutomaticoAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void radioManualAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGuardarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBuscarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnActualizarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEliminarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLimpiarAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSalirAction(ActionEvent event) {
+
+    }
+
+}
