213908ee85a948b582605384d63af878ab80c1cc
JuanHG07 <juan.hernandezg@uqvirtual.edu.co>
2024-11-17 00:04:22 -0500

    Update All

--- conceseionariofx/src/main/java/co/edu/uniquindio/poo/viewController/RestablecerViewController.java
+++ conceseionariofx/src/main/java/co/edu/uniquindio/poo/viewController/RestablecerViewController.java
@@ -4,9 +4,11 @@ import co.edu.uniquindio.poo.App;
 import co.edu.uniquindio.poo.controller.RestablecerController;
 import co.edu.uniquindio.poo.model.Administrador;
 import co.edu.uniquindio.poo.model.Cliente;
+import co.edu.uniquindio.poo.model.Usuario;
 import co.edu.uniquindio.poo.model.Vendedor;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
@@ -33,6 +35,10 @@ public class RestablecerViewController {
     @FXML
     void initialize() {
         restablecerController = new RestablecerController(app.concesionario);
+    }
+
+    @FXML
+    void cargarPregunta(ActionEvent event) {
         obtenerPregunta();
     }
 
@@ -49,18 +55,90 @@ public class RestablecerViewController {
                 txtPregunta.setText(vendedor.getPreguntaRecuperacion());
             }
         } for (Administrador administrador : restablecerController.obtenerListaAdministradores()) {
-
+            if (administrador.getCuenta().equals(usuario)) {
+                txtPregunta.setText(administrador.getPreguntaRecuperacion());
+            }
         }
     }
 
     @FXML
     void confirmarRespuesta(ActionEvent event) {
+        verificarRespuesta();
+    }
 
+    private boolean verificarRespuesta() {
+        String respuesta = txtRespuesta.getText();
+        boolean centinela = false;
+
+        for (Cliente cliente : restablecerController.obtenerListaClientes()) {
+            if (cliente.getRespuestaRecuperacion().equals(respuesta)) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setHeaderText(null);
+                alert.setTitle("INFO.");
+                alert.setContentText("La respuesta es correcta. Puede ingresar la nueva contraseña.");
+                alert.showAndWait();
+                centinela = true;
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setHeaderText(null);
+                alert.setTitle("INFO.");
+                alert.setContentText("La respuesta es incorrecta.");
+                alert.showAndWait(); 
+            }
+        } for (Vendedor vendedor : restablecerController.obtenerListaVendedores()) {
+            if (vendedor.getRespuestaRecuperacion().equals(respuesta)) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setHeaderText(null);
+                alert.setTitle("INFO.");
+                alert.setContentText("La respuesta es correcta. Puede ingresar la nueva contraseña.");
+                alert.showAndWait();
+                centinela = true;
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setHeaderText(null);
+                alert.setTitle("INFO.");
+                alert.setContentText("La respuesta es incorrecta.");
+                alert.showAndWait(); 
+            }
+        } for (Administrador administrador : restablecerController.obtenerListaAdministradores()) {
+            if (administrador.getRespuestaRecuperacion().equals(respuesta)) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setHeaderText(null);
+                alert.setTitle("INFO.");
+                alert.setContentText("La respuesta es correcta. Puede ingresar la nueva contraseña.");
+                alert.showAndWait();
+                centinela = true;
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setHeaderText(null);
+                alert.setTitle("INFO.");
+                alert.setContentText("La respuesta es incorrecta.");
+                alert.showAndWait(); 
+            }
+        }
+        return centinela;
     }
 
     @FXML
     void regresarLogin(ActionEvent event) {
-
+        
+        String usuario = app.getLoginViewController().getUsuario();
+        
+        if (verificarRespuesta()) {
+            for (Cliente cliente : restablecerController.obtenerListaClientes()) {
+                if (cliente.getRespuestaRecuperacion().equals(respuesta)) {
+                    
+                }
+            } for (Vendedor vendedor : restablecerController.obtenerListaVendedores()) {
+                if (vendedor.getRespuestaRecuperacion().equals(respuesta)) {
+                    
+                }
+            } for (Administrador administrador : restablecerController.obtenerListaAdministradores()) {
+                if (administrador.getRespuestaRecuperacion().equals(respuesta)) {
+                    
+                }
+            }
+        }
     }
 
     public void setApp(App app) {
