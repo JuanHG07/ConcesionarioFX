6d4ae0bceea753083875ae26d27db55168f870d7
JuanHG07 <juan.hernandezg@uqvirtual.edu.co>
2024-11-17 16:45:28 -0500

    Update

--- conceseionariofx/src/main/java/module-info.java
+++ conceseionariofx/src/main/java/module-info.java
@@ -6,4 +6,6 @@ module co.edu.uniquindio.poo {
     exports co.edu.uniquindio.poo;
     exports co.edu.uniquindio.poo.viewController to javafx.fxml;
     opens co.edu.uniquindio.poo.viewController to javafx.fxml;
+    opens co.edu.uniquindio.poo.model to javafx.base;
+    exports co.edu.uniquindio.poo.model;
 }
